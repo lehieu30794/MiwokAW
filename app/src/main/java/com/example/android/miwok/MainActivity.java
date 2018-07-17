2a36962ffb755bbaaf3c25bf8726c1ed31f3ba4a
lehieu30794 <lehieu30794@gmail.com>
2018-07-17 09:34:08 -0600

    Create all necessary Activities

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         // Find the View that shows the numbers category
         TextView numbers = (TextView) findViewById(R.id.numbers);
 
-// Set a click listener on that View
+        // Set a click listener on that View
         numbers.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the numbers View is clicked on.
             @Override
