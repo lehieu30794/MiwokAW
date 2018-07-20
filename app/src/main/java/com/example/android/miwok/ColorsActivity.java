7027cef81f40d2cb59ef6cbc5344ee118b6cf821
lehieu30794 <lehieu30794@gmail.com>
2018-07-19 19:17:03 -0600

    Update from Station3

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,6 +2,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        ArrayList<Color> colors = new ArrayList<Color>();
+
+        colors.add(new Color ("red", "weṭeṭṭi"));
+        colors.add(new Color ("red", "weṭeṭṭi"));
+        colors.add(new Color ("red", "weṭeṭṭi"));
+        colors.add(new Color ("red", "weṭeṭṭi"));
+        colors.add(new Color ("red", "weṭeṭṭi"));
+        colors.add(new Color ("red", "weṭeṭṭi"));
+        colors.add(new Color ("red", "weṭeṭṭi"));
+        colors.add(new Color ("red", "weṭeṭṭi"));
+
+        ColorAdapter itemAdapter = new ColorAdapter(this, colors);
+
+        ListView listView = (ListView) findViewById(R.id.color_list);
+        listView.setAdapter(itemAdapter);
+
     }
 }
