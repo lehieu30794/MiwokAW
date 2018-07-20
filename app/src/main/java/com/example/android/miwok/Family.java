01a0ed39b0993e4044d0419b05f189a23996a624
lehieu30794 <lehieu30794@gmail.com>
2018-07-19 20:44:12 -0600

    Display Family list (all major steps have been done)

--- app/src/main/java/com/example/android/miwok/Family.java
+++ app/src/main/java/com/example/android/miwok/Family.java
@@ -1,4 +1,25 @@
 package com.example.android.miwok;
 
 public class Family {
+
+    private String mMiwokFamily;
+    private String mDefaultFamily;
+
+    public Family(String a, String b) {
+        mMiwokFamily = a;
+        mDefaultFamily = b;
+
+        /*Interesting! Cannot do it the reverse way like this
+
+        a = mMiwokFamily;
+        b = mDefaultFamily;*/
+    }
+
+    public String getMiwokFamily() {
+        return mMiwokFamily;
+    }
+
+    public String getDefaultFamily() {
+        return mDefaultFamily;
+    }
 }
--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -14,9 +14,12 @@ public class FamilyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
 
-        ArrayList<String> familyMembers = new ArrayList<String>();
+        ArrayList<Family> familyItems = new ArrayList<Family>();
 
-        familyMembers.add("father");
+
+        familyItems.add(new Family("Aasdf","Basdf"));
+
+      /*  familyMembers.add("father");
         familyMembers.add("mother");
         familyMembers.add("son");
         familyMembers.add("daughter");
@@ -25,11 +28,11 @@ public class FamilyActivity extends AppCompatActivity {
         familyMembers.add("older sister");
         familyMembers.add("younger sister");
         familyMembers.add("grandmother");
-        familyMembers.add("grandfather");
+        familyMembers.add("grandfather");*/
 
-
-        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, familyMembers);
+        FamilyAdapter itemAdapter = new FamilyAdapter(this,familyItems);
+        /*Interesting! Delete the data type before coding; probably because the FamilyAdapter was modified without requiring data type input
+        FamilyAdapter<familyList> itemAdapter = new ArrayAdapter<familyList>(this, familyList);*/
 
         ListView familyListView = (ListView) findViewById(R.id.family_list_view);
         familyListView.setAdapter(itemAdapter);
